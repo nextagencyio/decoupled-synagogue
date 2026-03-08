d54f3b6cd281d178259b4d8df0bab558e8eee378
Jay Callicott <jrcallicott@gmail.com>
2026-03-08 08:42:47 -0500

    fix: mark header-dependent routes as dynamic

--- app/services/page.tsx
+++ app/services/page.tsx
@@ -7,6 +7,7 @@ import Header from '../components/Header'
 import ServiceTimeCard from '../components/ServiceTimeCard'
 
 export const revalidate = 3600
+export const dynamic = 'force-dynamic'
 
 export const metadata: Metadata = {
   title: 'Service Times | Synagogue',
